f43152bc08b0649e743f09d776ef90f5ed8c7271
obanext <93115438+obanext@users.noreply.github.com>
2026-06-30 16:53:07 +0200

    Revert "refine parsed jason output"
    This reverts commit 00d554e6f4c1387820806e63e6d1aac70230d8dd.

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const detailOptions = [
   {
     label: "IST oba.nl parsed json detailpagina",
     description:
-      "itemdetaildata wordt aangeleverd via OCLC/WISE endpoints geformateerd naar oba.nl geparseerd json model",
+      "OCLC Wise data wordt geladen via de gekozen detail-endpoints en gemapt naar het OBA debug parsed JSON-model.",
     href: "/oba-detail-parsed-json/842851",
     status: "Beschikbaar",
   },
